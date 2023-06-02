d826c73df690ad10c0fe6c1a4ad72f4204dc7b3f
jiakevin <jia_ke@hotmail.com>
2023-06-02 12:54:51 -0400

    init project

---
+++ src/main/java/dev/jiakevin/secureinvoice/SecureinvoiceApplication.java
@@ -0,0 +1,13 @@
+package dev.jiakevin.secureinvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureinvoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureinvoiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jiakevin/secureinvoice/SecureinvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jiakevin.secureinvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureinvoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
